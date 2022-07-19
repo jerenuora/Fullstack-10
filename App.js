052a3cfabd631661629a07aeeed51dd535e5bf8c
Jere Nuora <jere.nuora@gmail.com>
2022-07-19 21:01:09 +0300

    10.3

--- App.js
+++ App.js
@@ -1,23 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/components/Main";
 
-export default function App() {
-  console.log("testi clog")
-  return (
-    <View style={styles.container}>
-      <Text>App toimii! Ja päivittyy myös
-        simulaattorissa
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Lang: {item.language}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+      <Text>Reviews: {item.reviewCount} </Text>
+    </View>
+  )
+}
+
+export default RepositoryItem
