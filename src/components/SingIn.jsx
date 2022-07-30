4e4081bae725efb8f01d58aea114f06444ffad3e
Jere Nuora <jere.nuora@gmail.com>
2022-07-30 09:53:00 +0300

    10.8 signin-form visuals implemented

--- src/components/SingIn.jsx
+++ src/components/SingIn.jsx
@@ -1,7 +1,82 @@
-import Text from './Text';
+import { View, Pressable, StyleSheet } from 'react-native'
+import Text from './Text'
+import FormikTextInput from './FormikTextInput'
+import { Formik } from 'formik'
 
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+
+    justifyContent: 'center',
+  },
+  form: {
+    flexGrow: 1,
+    alignItems: 'center',
+
+    padding: 10,
+  },
+  textInput: {
+    width: '100%',
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'grey',
+    margin: 5,
+  },
+  button: {
+    width: '100%',
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'grey',
+    margin: 5,
+    alignItems: 'center',
+    backgroundColor: 'blue',
+  },
+})
+const initialValues = {
+  name: '',
+  password: '',
+}
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <FormikTextInput
+          style={styles.textInput}
+          name="name"
+          placeholder="Username"
+                    placeholderTextColor="grey"
+
+        />
+        <FormikTextInput
+          style={styles.textInput}
+          secureTextEntry
+          name="password"
+          placeholder="Password"  
+          placeholderTextColor="grey"
+
+        />
+        <Pressable style={styles.button} onPress={onSubmit}>
+          <Text color="appBarText" fontWeight="bold">
+            Sign in
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
-};
+  const onSubmit = (values) => {
+    console.log(values)
+  }
 
-export default SignIn;
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
+}
+
+export default SignIn
