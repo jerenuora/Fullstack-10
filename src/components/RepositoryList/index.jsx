d459a2adba32aeece2104266e68dc924c97572bd
Jere Nuora <jere.nuora@gmail.com>
2022-09-02 14:05:58 +0300

    10.23 sorting repositories

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -2,7 +2,10 @@ import { FlatList, View, StyleSheet, Pressable } from 'react-native'
 import RepositoryItem from './RepositoryItem'
 import useRepositories from '../../hooks/useRepositories.js'
 import { useNavigate } from 'react-router-native'
-
+import { useState } from 'react'
+// import {Picker} from '@react-native-picker/picker';
+import { Button, Menu, Provider } from 'react-native-paper'
+import theme from '../../theme'
 const styles = StyleSheet.create({
   separator: {
     height: 10,
@@ -11,7 +14,7 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({ picker, repositories }) => {
   const navigate = useNavigate()
 
   const repositoryNodes = repositories
@@ -28,17 +31,94 @@ export const RepositoryListContainer = ({ repositories }) => {
   }
   return (
     <FlatList
+      ListHeaderComponent={picker}
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={PressableArea}
       keyExtractor={(item) => item.id}
+      stickyHeaderIndices={[0]}
     />
+  )
+}
+const Picker = (setOrder) => {
+  const [visible, setVisible] = useState(false)
+  const openMenu = () => setVisible(true)
+
+  const closeMenu = () => setVisible(false)
+  const onPressHandler = (order) => {
+    closeMenu()
+    setOrder(order)
+  }
+  return (
+    <Provider>
+      <View
+        style={{
+          padding: 10,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          backgroundColor: theme.colors.backgroundColor,
+          flex: 1 
+        }}
+      >
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={
+            <Button
+              contentStyle={{ flexDirection: 'row-reverse' }}
+              icon="sort"
+              onPress={openMenu}
+            >
+              Sort repositories
+            </Button>
+          }
+        >
+          <Menu.Item
+            onPress={() => {
+              onPressHandler({
+                orderBy: 'CREATED_AT',
+                orderDirection: 'DESC',
+              })
+            }}
+            title="Latest"
+          />
+          <Menu.Item
+            onPress={() => {
+              onPressHandler({
+                orderBy: 'RATING_AVERAGE',
+                orderDirection: 'DESC',
+              })
+            }}
+            title="Highest rated"
+          />
+          <Menu.Item
+            onPress={() => {
+              onPressHandler({
+                orderBy: 'RATING_AVERAGE',
+                orderDirection: 'ASC',
+              })
+            }}
+            title="Lowest rated"
+          />
+        </Menu>
+      </View>
+    </Provider>
   )
 }
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories()
-  return <RepositoryListContainer repositories={repositories} />
+  const [order, setOrder] = useState({
+    orderBy: 'CREATED_AT',
+    orderDirection: 'DESC',
+  })
+  const { repositories } = useRepositories(order)
+
+  return (
+    <RepositoryListContainer
+      picker={Picker(setOrder)}
+      repositories={repositories}
+    />
+  )
 }
 
 export default RepositoryList
--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -2,12 +2,15 @@ import { useQuery } from '@apollo/client'
 import { useState, useEffect } from 'react'
 import { GET_REPOSITORIES } from '../graphql/queries'
 
-const useRepositories = () => {
+const useRepositories = ({ orderBy, orderDirection }) => {
   const [repositories, setRepositories] = useState()
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
+    variables:{
+      orderBy,
+      orderDirection,
+    }
   })
-
   const fetchRepositories = async () => {
     if (!loading) {
       setRepositories(data.repositories)
